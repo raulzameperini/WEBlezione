6a10458f28d4ed6a320c5064f867fea61efaa289
raul.zamperini <raul.zamperini@marconirovereto.it>
2025-10-01 16:24:24 +0200

    c

---
+++ script.js
@@ -0,0 +1,19 @@
+/* var c = "miao";
+let a = 5;
+const FARADAY_CONSTANT = 9.64854;
+console.log(c);
+// ``-->Alt 96
+console.log(`Faraaday constant is ${FARADAY_CONSTANT}`);
+
+ */
+
+let temp = 34;
+let tp = temp;
+
+
+console.log(`Temp: ${temp}`);
+console.log(`Tp: ${tp}`);
+
+temp = 2;
+console.log(`Temp: ${temp}`);
+console.log(`Tp: ${tp}`);
